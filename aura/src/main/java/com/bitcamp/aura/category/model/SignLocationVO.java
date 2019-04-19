55698a1a46123a5a66ffa4e34754c04e0a24837f
choi94 <ehdgns94@icloud.com>
2019-04-19 12:48:57 +0900

    JPA & Hibernate Entity Sequence 수

--- aura/src/main/java/com/bitcamp/aura/category/model/SignLocationVO.java
+++ aura/src/main/java/com/bitcamp/aura/category/model/SignLocationVO.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,10 +24,12 @@ import lombok.ToString;
 public class SignLocationVO {
 	@Id
 	@Column(name="LOCATION_NUM")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="REG_LOCATION_SEQ")
+	@SequenceGenerator(sequenceName="REG_LOCATION_SEQ", initialValue=1, allocationSize=1, name="REG_LOCATION_SEQ")
 	private int num;
 	
 	@Column(name="LOCATION_NAME", length=30)
 	private String name;
+	
 	
 }
