f51d3b7961ae1185848d2b45fadfbb1df49d13a3
youspend8 <youspend8@gmail.com>
2019-04-25 17:50:32 +0900

    검색결과 출력 부분 완료

--- aura/src/main/java/com/bitcamp/aura/review/controller/ReviewController.java
+++ aura/src/main/java/com/bitcamp/aura/review/controller/ReviewController.java
@@ -1,17 +1,23 @@
 package com.bitcamp.aura.review.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bitcamp.aura.review.service.RestaurantService;
+
 @Controller
 @RequestMapping(value="/review")
 public class ReviewController {
+
+	@Autowired
+	private RestaurantService service;
 	
 	@RequestMapping(value="/list")
-	public String list() {
-
-		return "reviewList";
+	public String list(Model model) {
+		model.addAttribute("list", service.searchAll());
+		return "/reviewList";
 	}
 	
 	@RequestMapping(value="/post")
